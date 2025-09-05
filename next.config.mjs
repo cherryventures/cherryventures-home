bb844de8843879a695d8169da64362b99184a8c5
cherryventures <cherryventures969@gmail.com>
2025-09-05 20:58:37 +0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === "production";
+const repo = "cherryventures-home";
 
-// If this is a **project page** (github.com/<user>/<repo>), set your repo name:
-const repo = "cherryventures-home"; // <-- change to your repo name
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Static HTML export
+export default {
   output: "export",
-
-  // Next/Image without the image optimizer (required for static export)
   images: { unoptimized: true },
-
-  // For GitHub **Project Pages** only (NOT needed for <user>.github.io root sites)
+  trailingSlash: true,
   basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
-
-  // Helps SSG produce folder-per-page (`index.html`)—better for GH Pages
-  trailingSlash: true,
 };
-
-export default nextConfig;
